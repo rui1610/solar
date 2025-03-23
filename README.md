3317e78dc6465102213059b6e0e07f99a8e09997
Rui Nogueira <rui.nogueira1610@hotmail.com>
2025-03-23 20:09:42 +0000

    refactor: remove unused files and update README for configuration instructions

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# 
+# Solar
 
 Based on https://hoerli.net/openhab-sma-wechselrichter-via-modbus-anbinden/
+
+Your to dos:
+- Rename the file [secrets.rename_env](./config/openhab/secrets.rename_env) into secrets.env
+- Adapt the items properly
--- libs/constants/files.py
+++ libs/constants/files.py
@@ -5,8 +5,3 @@ ROOT = Path(__file__, "..", "..", "..").resolve()
 
 FOLDER_CONFIGS = Path(ROOT, "config")
 FILE_CONFIG_SECRETS = Path(FOLDER_CONFIGS, "openhab", "secrets.env")
-
-FILE_CONFIG_SMA_METADATA = Path(FOLDER_CONFIGS, "openhab", "sma_things.json")
-FILE_CONFIG_SMA_METADATA_SOURCE = Path(
-    ROOT, "resources", "sma_tripower", "parameterlist_de.html"
-)
