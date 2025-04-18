c8a13df2a0a3b1425a3783e9f35e41bfcf50ae9d
Rui Nogueira <rui.nogueira1610@hotmail.com>
2025-04-18 19:03:08 +0200

    refactor: move SMA device data files to docs directory

--- libs/constants/files.py
+++ libs/constants/files.py
@@ -4,5 +4,5 @@ from pathlib import Path
 ROOT = Path(__file__, "..", "..", "..").resolve()
 
 FOLDER_CONFIGS = Path(ROOT, "config")
-FOLDER_DATA_DEVICES_SMA = Path(ROOT, "data", "devices", "sma")
+FOLDER_DATA_DEVICES_SMA = Path(ROOT, "docs", "devices", "sma")
 FILE_CONFIG_SECRETS = Path(FOLDER_CONFIGS, "openhab", "secrets.env")
