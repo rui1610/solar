14d457d3970ebc64698afff72a0382f77c13e1f0
Rui Nogueira <rui.nogueira@sap.com>
2025-04-24 23:38:55 +0200

    add ipython and pandas to requirements; create constants for chart dimensions and styles

---
+++ libs/constants/charts.py
@@ -0,0 +1,31 @@
+# Description: Constants for charts
+
+# Constants for the width and height of the bar charts
+BARPLOT_WIDTH = 8
+BARPLOT_HEIGHT = 4
+
+# Constants for the width and height of the line charts
+LINEPLOT_WIDTH = 8
+LINEPLOT_HEIGHT = 4
+
+TEXT_SIZE_BAR_LABELS = 6
+TEXT_SIZE_X_AXIS = 6
+TEXT_SIZE_Y_AXIS = 6
+TEXT_SIZE_TITLE = 10
+
+LABELS_COLOR = "black"
+
+COLOR_PALETTE = [
+    "#89D1FF",
+    "#1B90FF",
+    "#0070F2",
+    "#002A86",
+    "#00144A",
+    "#EAECEE",
+    "#A9B4BE",
+    "#5B738B",
+    "#475E75",
+    "#1A2733",
+    "#12171C",
+    "#D1EFFF",
+]
